f1cf3571b43e4662b1468b6479fdfdc1218cccfc
Фаррух Вохидов <fvs610@gmail.com>
2022-07-05 12:19:17 +0300

    Проект документирован

--- task.py
+++ task.py
@@ -2,20 +2,30 @@ import json
 
 
 def writer(file_name, data):
-    
+    """
+        Записывает данные в файл.
+        Принимает название файла и данные для записи.
+    """
     with open(f'{file_name}.json', 'w') as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
 
 
 def open_file(file_name):
-    
+    """
+        Открывает файл и возвращает содержимое файла.
+        Принимает название файла и возврашает данные записанные в файл.
+    """
     with open(f'{file_name}.json', 'r') as file:
         data = json.load(file)
     return data
 
 
 def find_product(file_name, product_name):
-    
+    """
+        Поиск записи в файле.
+        Принимает название файла и название нужного продукта.
+        Возврашает id продукта.
+    """
     data = open_file(file_name)
     list_data = data['Product']
     for i, product in enumerate(list_data):
@@ -25,7 +35,10 @@ def find_product(file_name, product_name):
 
 
 def add(file_name, product):
-    
+    """
+        Добавляет новый запись.
+        Принимает название файла и добавляемый продукт.
+    """
     data = open_file(file_name)
     data['Product'].append(product)
 
@@ -33,14 +46,21 @@ def add(file_name, product):
 
 
 def create_file(file_name, product_name, amount):
-    
+    """
+        Создает новый файл если нет файла с переданным именем.
+        Принимает название файла, название продукта и количество продукта.
+    """
     data = {'Product': [{product_name: amount}]}
 
     writer(file_name, data)
 
 
 def edit(file_name, product_name, amount):
-    
+    """
+        Редактирует запись в файле.
+        Принимает название файла, название редактируемого продукта и
+        количество.
+    """
     product_idx = find_product(file_name, product_name)
     data = open_file(file_name)
     data['Product'][product_idx][product_name] = amount
@@ -49,7 +69,10 @@ def edit(file_name, product_name, amount):
 
 
 def delete(file_name, product_name):
-    
+    """
+        Удвляет запись из файла.
+        Принимает название файла и название продукта которую надо удалить.
+    """
     product_idx = find_product(file_name, product_name)
     data = open_file(file_name)
     data['Product'].pop(product_idx)
@@ -58,7 +81,10 @@ def delete(file_name, product_name):
 
 
 def summ(file_name):
-   
+    """
+        Считает суммы всех продуктов.
+        Принимает название файла, результат печатает на консоль.
+    """
     data = open_file(file_name)
     list_data = data['Product']
     summ = 0
